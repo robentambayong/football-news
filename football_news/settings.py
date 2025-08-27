e700d422304b9ab39a886539303a187622373868
robentambayong <tambayongroben@gmail.com>
2025-08-27 15:30:01 +0700

    Update README

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 ...
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "roben-joseph-footballnews.pbp.cs.ui.ac.id"]
 ...
 
 # Application definition
