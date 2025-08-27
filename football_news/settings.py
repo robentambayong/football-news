462b137dc2a0ca19d9892dc038d55c125cffc0e5
robentambayong <tambayongroben@gmail.com>
2025-08-27 16:38:36 +0700

    Deploy project to PWS

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,11 +32,10 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-...
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "roben-joseph-footballnews.pbp.cs.ui.ac.id"]
-...
+ALLOWED_HOSTS = ["localhost",
+                 "127.0.0.1",
+                 "roben-joseph-footballnews.pbp.cs.ui.ac.id",
+]
 
 # Application definition
 
